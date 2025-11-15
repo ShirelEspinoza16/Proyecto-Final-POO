956c994f3d75666f4b2768b6ecc190b6cb1d89db
ShirelEspinoza16 <25100721@ue.edu.pe>
2025-11-15 15:40:56 -0500

    Commit total

--- REPASO PC02/ProyectoFinalPOO/src/main/java/BEAN/Empleados.java
+++ REPASO PC02/ProyectoFinalPOO/src/main/java/BEAN/Empleados.java
@@ -1,17 +1,78 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BEAN;
-
-/**
- *
- * @author Shirel
- */
+import java.util.Date;
 public class Empleados {
     private int empleadoID;
     private String nombre;
-    
-    
+    private String apellido;
+    private String cargo;
+    private Date fechaContratacion;
+    private String telefono;
+    private int estado;
+    public Empleados() {
+    }
+    public Empleados(int empleadoID, String nombre, String apellido, String cargo, Date fechaContratacion, String telefono, int estado) {
+        this.empleadoID = empleadoID;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cargo = cargo;
+        this.fechaContratacion = fechaContratacion;
+        this.telefono = telefono;
+        this.estado = estado;
+    }
+
+    public int getEmpleadoID() {
+        return empleadoID;
+    }
+
+    public void setEmpleadoID(int empleadoID) {
+        this.empleadoID = empleadoID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(Date fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
 }
--- REPASO PC02/ProyectoFinalPOO/src/main/java/DAO/EmpleadosDAO.java
+++ REPASO PC02/ProyectoFinalPOO/src/main/java/DAO/EmpleadosDAO.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author Shirel
- */
+import BEAN.Cliente;
+import UTIL.DbBean;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Vector;
+
 public class EmpleadosDAO {
+        Empleado  = new Vector();
+    }
     
+    public Vector<Empleado> listaEmpleado(String cad){
+        DbBean con;
+        con = new DbBean();
+        Vector<Empleado> listaEmpleados;
+        listaEmpleados = new Vector<Empleado>();
+        String sql; // Se crea recien el sql que se enviara a ejecutar
+        sql = "select * from Cliente";
+        if(!cad.isEmpty()){
+            sql = sql + " where apellidos like '"+cad + "%' or nombres like '"+ cad +"%'";
+        }
+        try{
+            ResultSet result;
+            result = con.resultadoSQL(sql);
+            while (result.next()){
+                Empleado emp = new Empleado();
+                emp.setEmpleadoID(result.getInt(1));
+                emp.setNombre(result.getString(2));
+                emp.setApellido(result.getString(3));
+                emp.setCargo(result.getString(4));
+                emp.setFechaContratacion(result.getString(5));
+                emp.setTelefono(result.getString(6));
+                emp.setEstado(result.getInt(7));
+                listaEmpleados.addElement(emp);
+            }
+        }catch(java.sql.SQLException e){
+            e.printStackTrace();
+        }
+        try{
+            con.desconecta();
+        }catch(java.sql.SQLException e){
+            e.printStackTrace();
+        }
+        return listaEmpleados;
+    } 
 }
